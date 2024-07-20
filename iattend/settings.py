50d4a31963d2ed84d3aafd2364f2ca055c3bd727
Mostafa Elshwaihi <128544423+elshwaihy@users.noreply.github.com>
2024-07-21 02:22:13 +0300

    Update settings.py

--- iattend/settings.py
+++ iattend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-4cfp@ddic=grzdqga&dvdudxi365v2=lh=$d19edzw&w93q-g2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
